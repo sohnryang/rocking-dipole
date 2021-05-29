69a573b9bc0f4ce594334e95ba8d41ff9c1f19da
Ryang Sohn <loop.infinitely@gmail.com>
2021-05-30 08:36:13 +0900

    Add basic renderer for dipoles

---
+++ src/renderer.ts
@@ -0,0 +1,55 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const lights = [
+  new THREE.PointLight(0xffffff, 1, 0),
+  new THREE.PointLight(0xffffff, 1, 0),
+  new THREE.AmbientLight(0x7f7f7f),
+];
+lights[0].position.setZ(5);
+lights[0].castShadow = true;
+lights[1].position.setZ(-5);
+lights[1].castShadow = true;
+for (const light of lights) scene.add(light);
+
+const redMaterial = new THREE.MeshLambertMaterial({ color: 0xcf3b36 });
+const geometry = new THREE.SphereGeometry(1, 32, 32);
+const positiveCharge = new THREE.Mesh(geometry, redMaterial);
+positiveCharge.position.setX(5);
+scene.add(positiveCharge);
+
+const blueMaterial = new THREE.MeshLambertMaterial({ color: 0x337ade });
+const negativeCharge = new THREE.Mesh(geometry, blueMaterial);
+negativeCharge.position.setX(-5);
+scene.add(negativeCharge);
+
+const material = new THREE.MeshLambertMaterial({ color: 0xffffff });
+const barGeometry = new THREE.BoxGeometry(10, 0.1, 0.1);
+const bar = new THREE.Mesh(barGeometry, material);
+scene.add(bar);
+
+camera.position.z = 10;
+
+function updatePosition(theta: number) {
+  positiveCharge.position.set(5 * Math.cos(theta), 5 * Math.sin(theta), 0);
+  negativeCharge.position.set(-5 * Math.cos(theta), -5 * Math.sin(theta), 0);
+  bar.rotation.z = theta;
+}
+
+let theta = 0.0;
+function animate() {
+  updatePosition((theta += 0.01));
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
